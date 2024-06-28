495387ac9a0a31503ecb65b04dda3cad06bc081b
dhruvgupta-14 <154119184+dhruvgupta-14@users.noreply.github.com>
2024-06-28 08:19:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+document.querySelector(".footer-a button").addEventListener("click", function() {
+  window.location.href = "index2.html"; 
+});
+document.querySelector("#footer-language ").addEventListener("click", function() {
+  alert('Sorry,This Option Is not Enabled'); 
+});
+document.querySelector("#priceimg button ").addEventListener("click", function() {
+  alert('Sorry,This Option Is not Enabled'); 
+});
+
---
+++ index2.js
@@ -0,0 +1,4 @@
+alert('This is not a course page \n\n You can go course page by Clicking Get Started')
+document.querySelector(".live ").addEventListener("click", function() {
+  alert('Sorry,This Option Is not Enabled'); 
+});
